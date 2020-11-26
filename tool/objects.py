e9341cb17a06458f210bfb0ea0a7dac4c016cd49
Fien Pockelé <j.s.pockele@student.tudelft.nl>
2020-11-26 15:30:58 +0100

    Added classes for all currently foreseen parts

--- tool/objects.py
+++ tool/objects.py
@@ -2,61 +2,210 @@
 Definition of all the object classes for the drag tool
 """
 
+import numpy as np
 
 from .equations import drag_coefficient_function
 
 
-class Object:
+class Part:
     """
     The general aerodynamic model for all objects defined below
     """
-    def __init__(self, drag_range: tuple, reynolds_number: int, position: tuple):
+
+    def __init__(self, drag_range: tuple, reynolds_number: int, dynamic_pressure: float,
+                 position: tuple):
+        self.dynamic_pressure = dynamic_pressure
         self.drag_coefficient = drag_coefficient_function(reynolds_number, drag_range)
 
+        self.position = position
         self.x_centre = position[0]
         self.y_centre = position[1]
         self.z_centre = position[2]
 
+    def frontal_surface(self, axis_1: int, axis_2: int):
+        raise NotImplementedError("Cannot execute for base class Part")
 
-class Sphere(Object):
+    def smallest_coordinate(self, axis: int):
+        raise NotImplementedError("Cannot execute for base class Part")
+
+    def calculate_base_drag(self, direction: int):
+        raise NotImplementedError("Cannot execute for base class Part")
+
+
+class Sphere(Part):
     """
     Aerodynamic model for a sphere
         The reference area is the frontal area
     """
     drag_range = (0.09, 0.18)
 
-    def __init__(self, reynolds_number: int, position: tuple):
-        super().__init__(self.drag_range, reynolds_number, position)
+    def __init__(self, reynolds_number: int, dynamic_pressure: float,
+                 position: tuple, radius: float):
+        self.radius = radius
+
+        super().__init__(self.drag_range, reynolds_number, dynamic_pressure, position)
+
+    def frontal_surface(self, axis_1: int, axis_2: int):
+        """
+        Determine the frontal surface on a given plane
+        :param axis_1: First axis defining the plane
+        :param axis_2: Second axis defining the plane
+        :return: (The shape of the surface,
+                The centre,
+                The radius,
+                The surface area)
+        """
+        centre = self.position[axis_1], self.position[axis_2]
+        return "circle", centre, self.radius, np.pi * self.radius ** 2
+
+    def smallest_coordinate(self, axis: int):
+        return self.position[axis] - self.radius
+
+    def calculate_base_drag(self, direction: int):
+        return self.drag_coefficient * (np.pi * self.radius ** 2) * self.dynamic_pressure
 
 
-class Cylinder(Object):
+class Cylinder(Part):
     """
     Aerodynamic model for a cylinder
         The reference area is the frontal area
+        Orientation indicates along which axis the cylinder is aligned
     """
     drag_range = (0.3, 0.7)
 
-    def __init__(self, reynolds_number: int, position: tuple):
-        super().__init__(self.drag_range, reynolds_number, position)
+    def __init__(self, reynolds_number: int, dynamic_pressure: float,
+                 position: tuple, radius: float, length: float, orientation: int):
+        self.radius = radius
+        self.length = length
+        self.orientation = orientation
+
+        super().__init__(self.drag_range, reynolds_number, dynamic_pressure, position)
+
+    def frontal_surface(self, axis_1: int, axis_2: int):
+        """
+        Determine the frontal surface on a given plane
+        :param axis_1: First axis defining the plane
+        :param axis_2: Second axis defining the plane
+        :return: (The shape of the surface,
+                Either the centre or the top left corner,
+                Either the radius or the bottom right corner,
+                The surface area)
+        """
+        if self.orientation == axis_1:
+            top_left = (self.position[axis_1] - self.length / 2,
+                        self.position[axis_2] + self.radius
+                        )
+            bottom_right = (self.position[axis_1] + self.length / 2,
+                            self.position[axis_2] - self.radius
+                            )
+
+            return "rectangle", top_left, bottom_right, 2 * self.length * self.radius
+
+        elif self.orientation == axis_2:
+            top_left = (self.position[axis_1] - self.radius,
+                        self.position[axis_2] + self.length / 2
+                        )
+            bottom_right = (self.position[axis_1] + self.radius,
+                            self.position[axis_2] - self.length / 2
+                            )
+
+            return "rectangle", top_left, bottom_right, 2 * self.length * self.radius
+
+        else:
+            centre = self.position[axis_1], self.position[axis_2]
+            return "circle", centre, self.radius, np.pi * self.radius ** 2
+
+    def smallest_coordinate(self, axis: int):
+        if self.orientation == axis:
+            return self.position[axis] - self.length / 2
+
+        else:
+            return self.position[axis] - self.radius
+
+    def calculate_base_drag(self, direction: int):
+        if direction == self.orientation:
+            return 0.9 * (np.pi * self.radius ** 2) * self.dynamic_pressure
+        else:
+            return self.drag_coefficient * (2 * self.length * self.radius) * self.dynamic_pressure
 
 
-class Cuboid(Object):
+class Cuboid(Part):
     """
     Aerodynamic model for a cuboid part
         The reference area is the frontal area
     """
     drag_range = (0.9, 0.9)
 
-    def __init__(self, reynolds_number: int, position: tuple):
-        super().__init__(self.drag_range, reynolds_number, position)
+    def __init__(self, reynolds_number: int, dynamic_pressure: float,
+                 position: tuple, dimensions: tuple):
+        self.dimensions = dimensions
+
+        super().__init__(self.drag_range, reynolds_number, dynamic_pressure, position)
+
+    def frontal_surface(self, axis_1: int, axis_2: int):
+        top_left = (self.position[axis_1] - self.dimensions[axis_1] / 2,
+                    self.position[axis_2] + self.dimensions[axis_2] / 2
+                    )
+
+        bottom_right = (self.position[axis_1] + self.dimensions[axis_1] / 2,
+                        self.position[axis_2] - self.dimensions[axis_2] / 2
+                        )
+
+        return ("rectangle", top_left, bottom_right,
+                self.dimensions[axis_1] * self.dimensions[axis_2]
+                )
+
+    def smallest_coordinate(self, axis: int):
+        return self.position[axis] - self.dimensions[axis] / 2
+
+    def calculate_base_drag(self, direction: int):
+        [axis_1, axis_2] = [axis for axis in [0, 1, 2] if axis != direction]
+        area = self.dimensions[axis_1] * self.dimensions[axis_2]
+        return self.drag_coefficient * area * self.dynamic_pressure
 
 
-class Disk(Object):
+class Dorito(Part):
     """
-    Aerodynamic model for a disk-like part
-        The reference area is the disk surface area
+    Aerodynamic model specially for the dorito of concept 4
+        The reference area is the surface area of the dorito
+        The dorito is always parallel to the 02-plane
     """
     drag_range = (0.05, 0.05)
 
-    def __init__(self, reynolds_number: int, position: tuple):
-        super().__init__(self.drag_range, reynolds_number, position)
+    def __init__(self, reynolds_number: int, dynamic_pressure: float,
+                 position: tuple, length: float, width: float, orientation: int):
+        if orientation == 1:
+            raise ValueError(f"Incompatible orientation of a dorito: {orientation}")
+        self.length = length
+        self.width = width
+        self.orientation = orientation
+        super().__init__(self.drag_range, reynolds_number, dynamic_pressure, position)
+
+    def frontal_surface(self, axis_1: int, axis_2: int):
+        if 1 not in (axis_1, axis_2):
+            raise ValueError(f"Can only calculate the frontal surface in the 01 and 12 planes")
+
+        elif self.orientation in (axis_1, axis_2):
+            datum = self.position[axis_1], self.position[axis_2]
+            return "flat_plate", datum, self.length, self.length * 0.005
+
+        else:
+            datum = self.position[axis_1], self.position[axis_2]
+            return "flat_plate", datum, self.width, self.width * 0.005
+
+    def smallest_coordinate(self, axis: int):
+        if axis == 1:
+            return self.position[axis]
+
+        elif axis == self.orientation:
+            return self.position[axis] - self.length
+
+        else:
+            return self.position[axis] - self.width / 2
+
+    def calculate_base_drag(self, direction: int):
+        if direction == 1:
+            raise ValueError(f"Incompatible flow direction: {direction}")
+
+        else:
+            return self.drag_coefficient * (0.5 * self.length * self.width) * self.dynamic_pressure
