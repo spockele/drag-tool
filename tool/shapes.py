bfcbf401bc382e79465ebb757374d74f8b91614b
Fien Pockelé <j.s.pockele@student.tudelft.nl>
2020-11-29 08:53:18 +0100

    Created the full logic for the rectangle-circle intersection

--- tool/shapes.py
+++ tool/shapes.py
@@ -17,8 +17,8 @@ class Rectangle:
 
         self.lines = (Line(self.left, self.top, self.right, self.top),
                       Line(self.left, self.bottom, self.right, self.bottom),
-                      Line(self.left, self.top, self.left, self.bottom),
-                      Line(self.right, self.top, self.right, self.bottom)
+                      Line(self.left, self.bottom, self.left, self.top),
+                      Line(self.right, self.bottom, self.right, self.top)
                       )
 
     def __repr__(self):
@@ -44,7 +44,10 @@ class Line:
     def __init__(self, x1, y1, x2, y2):
         self.x1, self.x2, self.y1, self.y2 = x1, x2, y1, y2
 
-        self.slope = 0 if self.x2 == self.x1 else (self.y2 - self.y1) / (self.x2 - self.x1)
+        self.slope = None if self.x2 == self.x1 else (self.y2 - self.y1) / (self.x2 - self.x1)
+
+    def __repr__(self):
+        return f"Line: [{self.x1}, {self.y1}, {self.x2}, {self.y2}, s={self.slope}]"
 
     def transform(self, shift_x, shift_y):
         return self.x1 + shift_x, self.y1 + shift_y, self.x2 + shift_x, self.y2 + shift_y
@@ -59,29 +62,37 @@ def sign(x):
 
 
 def line_length(x1, y1, x2, y2):
-    return np.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2)
+    return round(np.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2), 3)
 
 
 def intersection_line_circle(line: Line, circle: Circle):
     x1, y1, x2, y2 = line.transform(0 - circle.x_centre, 0 - circle.y_centre)
 
-    a = line.slope ** 2 + 1
-    b = 2 * (line.slope * y1 - x1 * line.slope ** 2)
-    c = (line.slope * x1 - y1) ** 2 - circle.radius ** 2
+    if line.slope is None and np.abs(x1) < circle.radius:
+        return (x1 + circle.x_centre, np.sqrt(circle.radius ** 2 - x1 ** 2) + circle.y_centre,
+                x2 + circle.x_centre, -np.sqrt(circle.radius ** 2 - x1 ** 2) + circle.y_centre)
 
-    discriminant = b ** 2 - 4 * a * c
+    elif line.slope is None:
+        return np.abs(x1) == circle.radius
 
-    if discriminant <= 0:
-        return discriminant == 0
     else:
-        x1_int = (np.sqrt(discriminant) - b) / (2 * a)
-        x2_int = (-np.sqrt(discriminant) - b) / (2 * a)
+        a = line.slope ** 2 + 1
+        b = 2 * (line.slope * y1 - x1 * line.slope ** 2)
+        c = (line.slope * x1 - y1) ** 2 - circle.radius ** 2
 
-        y1_int = line.slope * (x1_int - x1) + y1
-        y2_int = line.slope * (x2_int - x1) + y1
+        discriminant = b ** 2 - 4 * a * c
 
-        return (round(x1_int + circle.x_centre, 3), round(y1_int + circle.y_centre, 3),
-                round(x2_int + circle.x_centre, 3), round(y2_int + circle.y_centre, 3))
+        if discriminant <= 0:
+            return discriminant == 0
+        else:
+            x1_int = (np.sqrt(discriminant) - b) / (2 * a)
+            x2_int = (-np.sqrt(discriminant) - b) / (2 * a)
+
+            y1_int = line.slope * (x1_int - x1) + y1
+            y2_int = line.slope * (x2_int - x1) + y1
+
+            return (round(x1_int + circle.x_centre, 3), round(y1_int + circle.y_centre, 3),
+                    round(x2_int + circle.x_centre, 3), round(y2_int + circle.y_centre, 3))
 
 
 def area_circular_segment(intersection, circle: Circle):
@@ -94,10 +105,90 @@ def area_circular_segment(intersection, circle: Circle):
 
 def area_triangle(a, b, c):
     s = (a + b + c) / 2
-    return np.sqrt(s * (s - a) * (s - b) * (s - c))
+    return round(np.sqrt(s * (s - a) * (s - b) * (s - c)), 3)
+
+
+def on_line(line: Line, x, y):
+    return line.x1 <= x <= line.x2 and line.y1 <= y <= line.y2
 
 
 def intersection_rectangle_circle(rectangle: Rectangle, circle: Circle):
+    inside_vertices, outside_vertices = find_inside_vertices(circle, rectangle)
+    area = 0
+
+    if len(inside_vertices) == 0:
+        intersection, _ = find_intersection(circle, rectangle)
+
+        if len(intersection) == 4:
+            area = area_circular_segment(intersection, circle)
+
+        elif len(intersection) == 8:
+            circular_area1 = area_circular_segment(intersection[0:4], circle)
+            circular_area2 = area_circular_segment(intersection[4:8], circle)
+
+            area = circle.area - circular_area1 - circular_area2
+
+        elif (rectangle.left < circle.x_centre < rectangle.right
+              and rectangle.bottom < circle.y_centre < rectangle.top):
+            area = circle.area
+
+        else:
+            area = 0
+
+    elif len(inside_vertices) == 1:
+        intersection, _ = find_intersection(circle, rectangle)
+
+        circular_area = area_circular_segment(intersection, circle)
+        p1 = intersection[0:2]
+        p2 = intersection[2:4]
+        p3 = inside_vertices[0]
+
+        lengths = line_length(*intersection), line_length(*p2, *p3), line_length(*p1, *p3)
+        triangle_area = area_triangle(*lengths)
+
+        area = circular_area + triangle_area
+
+    elif len(inside_vertices) == 2:
+        intersection, intersect_lines = find_intersection(circle, rectangle)
+
+        circular_area = area_circular_segment(intersection, circle)
+
+        p1 = intersection[0], intersection[1]
+        p2 = intersection[2], intersection[3]
+
+        p3 = inside_vertices[0]
+        p4 = inside_vertices[1]
+
+        a1 = line_length(*intersection)
+        b1 = line_length(*p1, *p4)
+        c1 = line_length(*p2, *p4)
+        triangle_area1 = area_triangle(a1, b1, c1)
+
+        a2 = line_length(*p1, *p3)
+        b2 = line_length(*p1, *p4)
+        c2 = line_length(*p3, *p4)
+        triangle_area2 = area_triangle(a2, b2, c2)
+
+        area = circular_area + triangle_area1 + triangle_area2
+
+    elif len(inside_vertices) == 3:
+        intersection, _ = find_intersection(circle, rectangle)
+
+        circular_area = area_circular_segment(intersection, circle)
+        a = line_length(*intersection)
+        b = line_length(intersection[0], intersection[1], *outside_vertices[0])
+        c = line_length(intersection[2], intersection[3], *outside_vertices[0])
+        triangle_area = area_triangle(a, b, c)
+
+        area = rectangle.area - triangle_area + circular_area
+
+    else:
+        area = rectangle.area
+
+    return round(area, 3)
+
+
+def find_inside_vertices(circle, rectangle):
     dx2_left = (rectangle.left - circle.x_centre) ** 2
     dx2_right = (rectangle.right - circle.x_centre) ** 2
     dy2_top = (rectangle.top - circle.y_centre) ** 2
@@ -107,17 +198,36 @@ def intersection_rectangle_circle(rectangle: Rectangle, circle: Circle):
                  np.sqrt(dx2_left + dy2_bottom), np.sqrt(dx2_right + dy2_bottom)
                  )
 
-    inside_vertices = len([distance for distance in distances if distance < circle.radius])
+    vertices = ((rectangle.left, rectangle.top), (rectangle.right, rectangle.top),
+                (rectangle.left, rectangle.bottom), (rectangle.right, rectangle.bottom)
+                )
 
-    print(inside_vertices)
+    inside_vertices = [vertices[i] for i, distance in enumerate(distances)
+                       if distance < circle.radius]
 
-    if inside_vertices == 0:
-        intersections = []
-        for line in rectangle.lines:
-            intersections.append(intersection_line_circle(line, circle))
+    outside_vertices = [vertices[i] for i, distance in enumerate(distances)
+                        if not distance < circle.radius]
 
-        intersections = [intersection for intersection in intersections
-                         if not isinstance(intersection, bool)]
+    return inside_vertices, outside_vertices
 
-        if intersections:
-            return area_circular_segment(intersections[0], circle)
+
+def find_intersection(circle, rectangle):
+    intersection = []
+    intersect_lines = []
+
+    for line in rectangle.lines:
+        intersect = intersection_line_circle(line, circle)
+
+        if isinstance(intersect, tuple):
+
+            if on_line(line, intersect[0], intersect[1]):
+                intersection.append(intersect[0])
+                intersection.append(intersect[1])
+                intersect_lines.append(line)
+
+            if on_line(line, intersect[2], intersect[3]):
+                intersection.append(intersect[2])
+                intersection.append(intersect[3])
+                intersect_lines.append(line)
+
+    return intersection, intersect_lines
--- tool/test/test_shapes.py
+++ tool/test/test_shapes.py
@@ -25,6 +25,13 @@ class TestIntersectionLineCircle(unittest.TestCase):
 
         self.assertEqual(intersection, expected_intersection)
 
+        line = shapes.Line(0, 0, 0, 2)
+        circle = shapes.Circle(0, 1, 0.5)
+
+        intersection = shapes.intersection_line_circle(line, circle)
+
+        self.assertEqual(intersection, (0, 1.5, 0, 0.5))
+
     def test_tangent(self):
         line = shapes.Line(0, 0, 2, 0)
         circle = shapes.Circle(1, 1, 1)
@@ -43,13 +50,67 @@ class TestIntersectionLineCircle(unittest.TestCase):
 
 
 class TestIntersectionRectangleCircle(unittest.TestCase):
-    def test_no_inside_intersection(self):
+    def test_no_vertex_with_intersection(self):
         rectangle = shapes.Rectangle(0, 2, 2, 0)
         circle = shapes.Circle(1, 0, 1)
 
         expected_area = round(circle.area / 2, 3)
 
         self.assertEqual(shapes.intersection_rectangle_circle(rectangle, circle), expected_area)
+
+    def test_no_vertex_double_intersection(self):
+        rectangle = shapes.Rectangle(-1, 1, 4, -4)
+        circle = shapes.Circle(0, 0, 2)
+
+        expected_area = 4 * np.sqrt(3) + 2 * (circle.area * 1.047 / (2 * np.pi) - np.sqrt(3))
+
+        self.assertEqual(round(shapes.intersection_rectangle_circle(rectangle, circle), 2),
+                         round(expected_area, 2))
+
+        rectangle = shapes.Rectangle(-4, 4, 1, -1)
+        self.assertEqual(round(shapes.intersection_rectangle_circle(rectangle, circle), 2),
+                         round(expected_area, 2))
+
+    def test_no_vertex_circle_inside(self):
+        rectangle = shapes.Rectangle(-2, 2, 2, -2)
+        circle = shapes.Circle(0, 0, 1)
+
+        self.assertEqual(shapes.intersection_rectangle_circle(rectangle, circle),
+                         round(circle.area, 3))
+
+    def test_one_inside_vertex(self):
+        rectangle = shapes.Rectangle(0, 2, 2, 0)
+        circle = shapes.Circle(0, 0, 1)
+
+        self.assertEqual(shapes.intersection_rectangle_circle(rectangle, circle),
+                         round(0.25 * circle.area, 3))
+
+    def test_two_inside_vertex(self):
+        rectangle = shapes.Rectangle(0, 3, 1, 0)
+        circle = shapes.Circle(0, 0, 2)
+
+        helper_rectangle1 = shapes.Rectangle(0, 3, 3, 0)
+        helper_rectangle2 = shapes.Rectangle(0, 3, 3, 1)
+        expected_area = shapes.intersection_rectangle_circle(helper_rectangle1, circle) -\
+            shapes.intersection_rectangle_circle(helper_rectangle2, circle)
+
+        self.assertEqual(shapes.intersection_rectangle_circle(rectangle, circle), expected_area)
+
+    def test_three_inside_vertex(self):
+        rectangle = shapes.Rectangle(0, 1.8, 1.8, 0)
+        circle = shapes.Circle(0, 0, 2)
+
+        expected_area = rectangle.area - 0.431 + 0.1064 * circle.area - 1.2397
+
+        self.assertEqual(round(shapes.intersection_rectangle_circle(rectangle, circle), 2),
+                         round(expected_area, 2))
+
+    def test_four_inside_vertex(self):
+        rectangle = shapes.Rectangle(-1, 1, 1, -1)
+        circle = shapes.Circle(0, 0, 2)
+
+        self.assertEqual(shapes.intersection_rectangle_circle(rectangle, circle),
+                         round(rectangle.area, 3))
 
 
 class TestAreaCircularSegment(unittest.TestCase):
@@ -68,6 +129,23 @@ class TestAreaCircularSegment(unittest.TestCase):
         expected_area = round(0.25 * circle.area - 0.5 * np.sin(np.pi/4) * 2 * np.cos(np.pi/4), 3)
 
         self.assertEqual(shapes.area_circular_segment(intersection, circle), expected_area)
+
+
+class TestAreaTriangle(unittest.TestCase):
+    def test_equilateral(self):
+        sides = 1, 1, 1
+
+        self.assertEqual(shapes.area_triangle(*sides), round(0.25 * np.sqrt(3), 3))
+
+    def test_isosceles(self):
+        sides = 1, 2, 2
+
+        self.assertEqual(shapes.area_triangle(*sides), round(0.25 * np.sqrt(15), 3))
+
+    def test_other(self):
+        sides = 3, 4, 5
+
+        self.assertEqual(shapes.area_triangle(*sides), 6)
 
 
 if __name__ == '__main__':
