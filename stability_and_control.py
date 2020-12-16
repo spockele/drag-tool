f66db15d0324cff8181e484885e76ba5256a86f2
Fien Pockelé <j.s.pockele@student.tudelft.nl>
2020-12-16 08:20:41 +0100

    Removed the s and c thing, as it turns out to be impossible

--- stability_and_control.py
+++
@@ -1,7 +0,0 @@
-if __name__ == '__main__':
-    index = int(input('Enter the index of the input file: '))
-    f = open(f'data/s_and_c-input_{index}.csv')
-    lines = [line.strip(",\n").split(", ") for line in f.readlines()]
-    f.close()
-
-    print(lines)
